d0cf8bea89e0263bf11c30dd360fc8b7b27d565e
a-mhamdi <a_mhamdi@outlook.com>
2026-05-21 21:55:04 +0100

    polybar config

---
+++ files/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Make sure DISPLAY is set
+if [ -z "$DISPLAY" ]; then
+    export DISPLAY=:0
+fi
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+echo "---" | tee -a /tmp/polybar1.log
+polybar --config=~/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Polybar launched..."
